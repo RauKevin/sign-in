389f562000ed782d5ce378cd517f02f04b4a0bd5
RauKevin <kevin_rau87@yahoo.com>
2017-12-10 13:26:18 -0800

    front end admin view and signin components built

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="container">
     <h3>Administrator Portal</h3>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone #</th>
+        <th>Company</th>
+      </tr>
+      <!-- only loop through x at a time, then request more, from most recent -->
+      <tr v-for="user in users">
+        <td>{{user.name}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.phone}}</td>
+        <td>{{user.company}}</td>
 
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -10,12 +25,33 @@ export default {
   name: 'admin',
   data () {
     return {
-      msg: 'Sign in here!'
+      msg: 'Sign in here!',
+      users: [
+        {
+          name:'kevin',
+          email: 'mcLovin69@gmail.com',
+          phone: '123456789',
+          company: 'Hustler'
+        },
+        {
+          name:'chantal',
+          email: 'itouchpenis@mail.com',
+          phone: '123456789',
+          company: 'i heart jews'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+table, td, th {
+  border: 1px solid black;
+  padding: 1em;
+}
+table {
+  border-collapse: collapse;
+  margin: auto;
+}
 </style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,6 +2,24 @@
   <div id="container">
     <h3>{{msg}} </h3>
 
+      <input type="text" placeholder="Name" v-model="user.name"/>
+      <br>
+      <input type="email" placeholder="Email" v-model="user.email"/>
+      <br>
+      <input type="tel" placeholder="Telephone" v-model="user.phone"/>
+      <br>
+      <input type="text" placeholder="Company" v-model="user.company"/>
+      <br>
+      <input type="checkbox" value="office" v-model="user.officeVisit" />
+      <label> Official visit: </label>
+      <br>
+      <input type="checkbox" value="false" v-model="user.escort" />
+      <label> Escort required: </label>
+      <input v-if="user.escort" type="text" placeholder="Escourt Name" v-model="user.escourtName"/>
+      <br>
+      <button @click="Signin" >Sign in</button>
+
+    <br>
   </div>
 </template>
 
@@ -10,14 +28,59 @@ export default {
   name: 'sign-in',
   data () {
     return {
-      msg: 'Sign in here!'
+      msg: 'Sign in here!',
+      user: {
+        name: '',
+        email: '',
+        phone: '',
+        company: '',
+        officeVisit: false,
+        escort: false,
+        escourtName: ''
+      }
     }
+  },
+  methods: {
+    Signin()
+    {
+      console.log(this.user);
+      alert("Hello "+this.user.name + ", Sign in confirmed");
+      location.reload();
+    },
+    refresh()
+    {
+      this.user.name = '';
+      this.user.email = '';
+      this.user.phone = '';
+      this.user.company = '';
+      this.user.officeVisit = false;
+      this.user.escort = false;
+
+    }
+
   }
 }
 </script>
 
-<style>
+<style scoped>
 #container {
-    border: 1px black solid;
+  border: 3px solid #f1f1f1;
+}
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+input{
+  display: inline-block;
+  font-size: 1.5em;
 }
 </style>
