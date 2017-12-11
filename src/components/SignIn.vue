89fd194917564090bb6f3d1bb7a81f31ffb7504a
RauKevin <kevin_rau87@yahoo.com>
2017-12-10 18:28:18 -0800

    Form validation added to sign in

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="container">
     <h3>{{msg}} </h3>
-
+    <div v-show="!signedIn">
       <input type="text" placeholder="Name" v-model="user.name"/>
       <br>
       <input type="email" placeholder="Email" v-model="user.email"/>
       <br>
-      <input type="tel" placeholder="Telephone" v-model="user.phone"/>
+      <input type="tel" placeholder="Phone Number xxx-xxx-xxxx" v-model="user.phone" required
+           pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
       <br>
-      <input type="text" placeholder="Company" v-model="user.company"/>
+      <input type="text" placeholder="Company (optional)" v-model="user.company"/>
       <br>
       <input type="checkbox" value="office" v-model="user.officeVisit" />
       <label> Official visit: </label>
       <br>
       <input type="checkbox" value="false" v-model="escort" />
       <label> Escort required: </label>
+      <br>
       <input v-if="escort" type="text" placeholder="Escourt Name" v-model="user.escortName"/>
       <br>
       <button @click="Signin" >Sign in</button>
-
+    </div>
     <br>
   </div>
 </template>
@@ -29,6 +31,7 @@ export default {
   data () {
     return {
       msg: 'Sign in here!',
+      signedIn: false,
       escort: false,
       user: {
         name: '',
@@ -44,25 +47,35 @@ export default {
     Signin()
     {
       console.log(this.user);
-            
+
+      //insert user data validation here
+      if(this.validate())
+      {
+        alert("the data is good!");
+      }
+      /*      
       this.$http.post('http://localhost:4000/api/signin', this.user)
         .then((data) => {
           console.log(data.body);
           if(data.body.status == 'failure')
           {
-            this.msg = "Welcome " +data.body.email+", Registration successful!";
+            this.msg = "Welcome " +data.body+", Sign in successful!";
             console.log('Error');
             this.msg = data.body.err;
           }
           else{
-            this.msg = "Welcome " +data.body.email+", Registration successful!";
+            this.msg = "Welcome " +data.body+", Sign in successful!";
             alert("Hello "+this.user.name + ", Sign in confirmed");
             // this.$router.push('/');
           }
         });
+      */
 
       //clear form data after sign in  
-      this.refresh(); //make this async to refresh after a set timeout
+      //this.refresh();
+      this.signedIn = true;
+      this.msg = "Welcome " +this.user.name+", Sign in successful!";
+      setTimeout(this.refresh, 4000);
     },
     refresh()
     {
@@ -73,6 +86,48 @@ export default {
       this.user.officeVisit = false;
       this.user.escort = false;
       this.user.escourtName = '';
+
+      this.msg = 'Sign in here!';
+      this.signedIn = false;
+    },
+    validate()
+    {
+      let valid = true; //can also make an error message
+
+      //check name
+      if(this.user.name.length < 2 || this.user.name.length > 32)
+      {
+        valid = false;
+      }
+      else{
+        //check for invalid characters
+        valid = !/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.user.name);
+      }
+
+      //check email
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      valid = re.test(this.user.email);
+
+      //check phone - checked in form field
+
+      //check escourt
+      if(this.escort)
+      {
+        if(this.user.escourtName.length < 2 || this.user.escourtName.length > 32)
+        {
+          valid = false;
+        }
+        else{
+          //check for invalid characters
+          valid = !/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.user.escourtName);
+        }
+      }
+      else{
+        this.user.escourtName = '';
+      }
+      
+
+      return valid;
     }
 
   }
@@ -82,6 +137,7 @@ export default {
 <style scoped>
 #container {
   border: 3px solid #f1f1f1;
+  background-color: #f1f1f1;
 }
 button {
     background-color: #4CAF50;
@@ -90,7 +146,7 @@ button {
     margin: 8px 0;
     border: none;
     cursor: pointer;
-    width: 100%;
+    width: 50%;
 }
 
 button:hover {
@@ -98,6 +154,8 @@ button:hover {
 }
 input{
   display: inline-block;
-  font-size: 1.5em;
+  font-size: 1.2em;
+  margin-bottom: 10px;
 }
+
 </style>
